efb54676ac7bfcc2fe3466be66d28874b174168e
Mitrovic-20 <151095299+Mitrovic-20@users.noreply.github.com>
2023-11-17 02:31:10 +0200

    Create Spatial Join.java

---
+++ Spatial Join.java
@@ -0,0 +1,2 @@
+Joins attributes from one feature to another based on the spatial relationship. The target features and the joined attributes from the join features are written to the output feature class.
+
